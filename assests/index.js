b4c5378dacfce7781801872c16d31da80f4d5bc9
Jassi88 <63803662+jagroop2388@users.noreply.github.com>
2024-09-18 22:13:12 -0600

    Add files via upload

---
+++ assests/index.js
@@ -0,0 +1,8 @@
+document.getElementById('nav-item').addEventListener('click', function() {
+    window.location.href = 'nav-item'; // Change this to your actual about page URL
+});
+
+// Optional: Function to programmatically click the button
+function clickAboutSection() {
+    document.getElementById('nav-item').click();
+}
